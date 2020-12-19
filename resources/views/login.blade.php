305c0b5606795f6a85dc8d667cfe74d31dec2672
Evgeny Karachakov <evgdev@yandex.ru>
2020-12-19 23:54:25 +0700

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,14 +3,15 @@
 <div class="container custom-login">
 	<div class="row">
 		<div class="col-sm-4 col-sm-offset-4">
-	<form class="form-signin">
+	<form class="form-signin" action="/login" method="POST">
 		<div class="form-group">
+			@csrf
 	      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
 	      <label for="inputEmail" >Email address</label>
-	      <input type="email" id="inputEmail" class="form-control mb-5" placeholder="Email address" required autofocus>
+	      <input type="email" id="inputEmail" name="email" class="form-control mb-5" placeholder="Email address" required autofocus>
 	      <div class="mt-5">
 	      <label for="inputPassword" class=" mt-5">Password</label>
-	      <input type="password" id="inputPassword" class="form-control mt-5" placeholder="Password" required>
+	      <input type="password" id="inputPassword" name="password" class="form-control mt-5" placeholder="Password" required>
 	      </div>
 	      <div class="checkbox mb-3">
 	        <label>
